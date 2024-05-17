355f3ab737d06f415948fb1fa6efab0587b776b2
EricBinek <ericbineksant@gmail.com>
2024-05-16 22:11:23 -0300

    Quadrados e fisica

--- script.js
+++ script.js
@@ -1,27 +1,34 @@
-var canvas = document.getElementById("canvas");
-var desenho = canvas.getContext("2d");
+let canvas = document.getElementById("canvas");
+let desenho = canvas.getContext("2d");
 
-var setaDireita = false;
-var setaEsquerda = false;
+let setaDireita = false;
+let setaEsquerda = false;
 
-var rebatedorAltura = 10;
-var rebatedorLargura = 70;
-var rebatedorX = (canvas.width - rebatedorLargura) / 2;
+let rebatedorAltura = 10;
+let rebatedorLargura = 70;
+let rebatedorX = (canvas.width - rebatedorLargura) / 2;
 
-var bolaRadius = 15;
-var bolaX = canvas.width / 2;
-var bolaY = canvas.height - rebatedorAltura - bolaRadius;
-var bolaDX = 6;
-var bolaDY = -2;
+let bolaRadius = 15;
+let bolaX = canvas.width / 2;
+let bolaY = canvas.height - rebatedorAltura - bolaRadius;
+let bolaDX = 6;
+let bolaDY = -2;
 
-var tijolosPorLinha = 5;
-var tijolosPorColuna = 3;
-var tijoloLargura = 75;
-var tijoloAltura = 25;
-var tijoloPadding = 10;
-var tijoloOffsetTop = 30;
-var tijoloOffsetLeft = 30;
-var tijolos = [];
+let tijolosPorLinha = 13;
+let tijolosPorColuna = 4;
+let tijoloLargura = 80;
+let tijoloAltura = 40; 
+let tijoloPadding = 10;
+let tijoloOffsetTop = (canvas.height - (tijolosPorColuna * (tijoloAltura + tijoloPadding))) / 3; 
+let tijoloOffsetLeft = (canvas.width - (tijolosPorLinha * (tijoloLargura + tijoloPadding))) / 2; 
+let tijolos = [];
+
+for (let c = 0; c < tijolosPorColuna; c++) {
+    tijolos[c] = [];
+    for (let r = 0; r < tijolosPorLinha; r++) {
+        tijolos[c][r] = { x: 0, y: 0, status: 1 };
+    }
+}
 
 document.addEventListener("keydown", descerTecla);
 document.addEventListener("keyup", subirTecla);
@@ -58,13 +65,47 @@ function desenharBola() {
     desenho.closePath();
 }
 
+function desenharTijolos() {
+    for (let c = 0; c < tijolosPorColuna; c++) {
+        for (let r = 0; r < tijolosPorLinha; r++) {
+            if (tijolos[c][r].status == 1) {
+                let tijoloX = (r * (tijoloLargura + tijoloPadding)) + tijoloOffsetLeft;
+                let tijoloY = (c * (tijoloAltura + tijoloPadding)) + tijoloOffsetTop;
+                tijolos[c][r].x = tijoloX;
+                tijolos[c][r].y = tijoloY;
+                desenho.beginPath();
+                desenho.rect(tijoloX, tijoloY, tijoloLargura, tijoloAltura);
+                desenho.fillStyle = "blue";
+                desenho.fill();
+                desenho.closePath();
+            }
+        }
+    }
+}
+
+function colisao() {
+    for (let c = 0; c < tijolosPorColuna; c++) {
+        for (let r = 0; r < tijolosPorLinha; r++) {
+            let t = tijolos[c][r];
+            if (t.status == 1) {
+                if (bolaX > t.x && bolaX < t.x + tijoloLargura && bolaY > t.y && bolaY < t.y + tijoloAltura) {
+                    bolaDY = -bolaDY;
+                    t.status = 0;
+                }
+            }
+        }
+    }
+}
+
 function desenhar() {
     desenho.clearRect(0, 0, canvas.width, canvas.height);
     desenharRebatedor();
     desenharBola();
+    desenharTijolos();
+    colisao(); // Check for collision with bricks
 
     if (bolaX + bolaDX > canvas.width - bolaRadius || bolaX + bolaDX < bolaRadius) {
-        bolaDX = - bolaDX;
+        bolaDX = -bolaDX;
     }
 
     if (bolaY + bolaDY < bolaRadius) {
